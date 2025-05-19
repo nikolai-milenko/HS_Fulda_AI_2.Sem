b64d76d75cfba3a26247b3c8728a50f6cc0df804
Nikolay Milenko <nikolay.milenko@gmail.com>
2025-05-19 13:11:53 +0200

    testat 2 finished

--- Programmierung 2/woche4/src/testat2/playground/BossLevel25.java
+++ Programmierung 2/woche4/src/testat2/playground/BossLevel25.java
@@ -1,8 +1,21 @@
 package testat2.playground;
 
+import javashooter.gameobjects.GameObject;
 import spaceinvadersProject.playground.SpaceInvadersLevel;
 
+import java.awt.*;
+
 public class BossLevel25 extends SpaceInvadersLevel {
+
+    public int getHitCounter() {
+        return hitCounter;
+    }
+
+    public void setHitCounter(int hitCounter) {
+        this.hitCounter = hitCounter;
+    }
+
+    private int hitCounter = 0;
 
     @Override
     protected int calcNrEnemies() {
@@ -27,5 +40,45 @@ public class BossLevel25 extends SpaceInvadersLevel {
     @Override
     protected String getStartupMessage() {
         return "BossLevel!!";
+    }
+
+    @Override
+    protected void actionIfEnemyIsHit(GameObject e, GameObject shot) {
+        setHitCounter(getHitCounter() + 1);
+
+        deleteObject(shot.getId());
+
+        if (getHitCounter() >= 20) {
+            deleteObject(e.getId());
+        }
+    }
+
+    @Override
+    public void redrawLevel(Graphics2D g2){
+        super.redrawLevel(g2);
+        g2.setFont(new Font("SansSerif", Font.BOLD, 20));
+        g2.setColor(Color.YELLOW);
+        g2.drawString("HitCounter: " + getHitCounter(), 120, 20);
+    }
+
+    @Override
+    protected GameObject createEnemyShot(GameObject e) {
+        GameObject shot = super.createEnemyShot(e);
+        if (shot == null) {
+            return null;
+        }
+        GameObject ego  = getObject("ego");
+
+        double dx = ego.getX() - shot.getX();
+        double dy = ego.getY() - shot.getY();
+        double n  = Math.sqrt(dx*dx + dy*dy);
+        double S  = 400.0;
+
+        double vx = dx * S / n;
+        double vy = dy * S / n;
+
+        shot.setVX(vx);
+        shot.setVY(vy);
+        return shot;
     }
 }
